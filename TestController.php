465574d0f37ad9feb26888f160990d3f78b7be04
Alessandro <sblombliz@users.noreply.github.com>
2018-07-15 17:14:51 +0200

    Update TestController.php

--- TestController.php
+++ TestController.php
@@ -4,12 +4,12 @@
 	READ! THIS FILE IS NOT NEEDED!
 
     Its just an Example Controller taken for laravel to show how i'm communicating with my RESTS, but you
-	can use your own REST method like a direct connection to database, just respect urls used
+    can use your own REST method like a direct connection to database, just respect urls used
     when initializing js class and the returning JSON format.
 	
 	In this example my routes are:
-	GET		index = "/users/"			-> return all users
-	PUSH	store = "/users/ 			-> Make a new user using request data
+	GET	index = "/users/"		-> return all users
+	PUSH	store = "/users/ 		-> Make a new user using request data
 	PATCH	update = "/users/{id}" 		-> Refresh user info (Id number needed on url)
 	DELETE  delete = "/users/{id}"		-> Delete the selected user (Id number needed on url)
 

234d0450411ad17a9e6d86eeed48005ea91d2ed5
Alessandro Cardia <cardia.alessandro@gmail.com>
2018-07-15 17:13:27 +0200

    Updated info on Example Controller

--- TestController.php
+++ TestController.php
@@ -1,79 +1,56 @@
 <?php
 
-namespace App\Http\Controllers;
+/*
+	READ! THIS FILE IS NOT NEEDED!
 
-use App\Perftiming;
-use Illuminate\Http\Request;
+    Its just an Example Controller taken for laravel to show how i'm communicating with my RESTS, but you
+	can use your own REST method like a direct connection to database, just respect urls used
+    when initializing js class and the returning JSON format.
+	
+	In this example my routes are:
+	GET		index = "/users/"			-> return all users
+	PUSH	store = "/users/ 			-> Make a new user using request data
+	PATCH	update = "/users/{id}" 		-> Refresh user info (Id number needed on url)
+	DELETE  delete = "/users/{id}"		-> Delete the selected user (Id number needed on url)
 
-use App\Http\Requests;
+*/
 
-class PerftimingController extends Controller
+class UserController
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        $perftimings = Perftiming::all();
-        return json_encode($perftimings);
+        $users = User::all();
+        return json_encode($users);
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+	
     public function store(Request $request)
     {
-        $perftiming = Perftiming::create($request->all());
-        $perftiming->save();
-
+        $user = User::create($request->all());
+		
         return response()->json([
             'fail' => false
         ]);
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+	
     public function show($id)
     {
-        $perftiming = Perftiming::find($id);
-        return json_encode($perftiming);
+        $user = User::find($id);
+        return json_encode($user);
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+	
     public function update(Request $request, $id)
     {
-        $perftiming = Perftiming::find($id);
-        $perftiming->update($request->all());
-        $perftiming->save();
+        $user = User::find($id);
+        $user->update($request->all());
+        $user->save();
 
         return response()->json([
             'fail' => false
         ]);
     }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+	
     public function destroy($id)
     {
-        Perftiming::destroy($id);
+        User::destroy($id);
     }
 }
